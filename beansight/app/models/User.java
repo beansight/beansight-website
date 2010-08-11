6bcb78ea8e57fcc42a77df641221867f21e4304b
jeanbaptiste <jeanbaptiste@2e3b6b4b-2413-4334-92b7-7e2fc515c0b6>
2010-08-11 22:54:40 +0000

    Des commentaires et du code poour suivre un insight
    git-svn-id: svn://92.243.5.254/srv/d_sterenfr/svn/beansight/Website/trunk@55 2e3b6b4b-2413-4334-92b7-7e2fc515c0b6

--- beansight/app/models/User.java
+++ beansight/app/models/User.java
@@ -10,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
+import exceptions.UserIsAlreadyFollowingInsightException;
+
 import models.Vote.State;
 
 import play.db.jpa.Model;
@@ -50,6 +52,14 @@ public class User extends Model {
         return userName;
     }
     
+    /**
+     * Call this method to authenticate a user given his username 
+     * and password.
+     * 
+     * @param username
+     * @param password
+     * @return true if authenticated, false otherwise
+     */
     public static boolean connect(String username, String password) {
     	User user = find("userName=? and password=?", username, Crypto.passwordHash(password)).first();
     	if (user!=null) {
@@ -59,11 +69,25 @@ public class User extends Model {
     	return false;
     }
 	
+    /**
+     * Static method to get a User instance given his username
+     * 
+     * @param userName
+     * @return
+     */
     public static User findByUserName(String userName) {
     	return find("userName = ?", userName).first();
     }
 
-    // TODO : get the end date.
+    /**
+     * Call this method to create a new insight that will be
+     * automatically owned by the current user.
+     * 
+     * TODO : get the end date.
+     * 
+     * @param insightContent
+     * @return
+     */
     public Insight createInsight(String insightContent) {
     	Date endDate = new Date();
     	Insight i = new Insight(this, insightContent, endDate);
@@ -73,7 +97,18 @@ public class User extends Model {
     	return i;
     }
     
-	public void voteToInsight(Insight insight, State voteState) {
+    /**
+     * Call this method to set a vote for one insight for the
+     * current user.
+     * It shouldn't be possible to vote twice for one insight.
+     * 
+     * TODO : check user hasn't already vote for the insight
+     * 
+     * @param insightId : id of the insight user is voting for.
+     * @param voteState State.AGREE or State.DISAGREE
+     */
+	public void voteToInsight(Long insightId, State voteState) {
+		Insight insight = Insight.findById(insightId);
 		Vote vote = new Vote(this, insight, voteState);
 		votes.add(vote);
 		if (voteState.equals(State.AGREE)) {
@@ -93,4 +128,44 @@ public class User extends Model {
 	}
 
 	
+	public boolean isFollowingInsight(Insight insight) {
+		if(followedInsights.contains(insight))
+			return true;
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param insightId
+	 */
+	public void startFollowingThisInsight(Long insightId) throws UserIsAlreadyFollowingInsightException {
+		Insight insight = Insight.findById(insightId);
+		
+		// If we are already following the insight throw a business exception
+		if (isFollowingInsight(insight)==true)
+			throw new UserIsAlreadyFollowingInsightException();
+		
+		followedInsights.add(insight);
+		save();
+		insight.followers.add(this);
+		insight.save();
+	}
+	
+	
+	/**
+	 * 
+	 * @param insightId
+	 */
+	public void stopFollowingThisInsight(Long insightId) {
+		Insight insight = Insight.findById(insightId);
+		
+		// If we were not following the insight just do nothing ...
+		if (isFollowingInsight(insight)==false)
+			return;
+		
+		followedInsights.remove(insight);
+		save();
+		insight.followers.remove(this);
+		insight.save();
+	}
 }
