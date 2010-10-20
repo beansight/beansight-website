0a20be221e082242f0f70faf69a2232474705396
steren <steren.giannini@gmail.com>
2010-10-20 15:22:29 +0000

    moved the vote code to a javascript file (because it needs to be shared)
    git-svn-id: svn://92.243.5.254/srv/d_sterenfr/svn/beansight/Website/trunk@110 2e3b6b4b-2413-4334-92b7-7e2fc515c0b6

---
+++ beansight/public/javascripts/beansight.js
@@ -0,0 +1,31 @@
+// Insight Vote
+
+/** Current user agree an insight */
+function agree(insightId) {
+    $.getJSON(agreeAction({'insightId': insightId}), onVoteSuccess);
+}
+
+/** Current user disagree an insight */
+function disagree(insightId) {
+    $.getJSON(disagreeAction({'insightId': insightId}), onVoteSuccess);
+}
+
+/** Callback after a vote is done */
+function onVoteSuccess(data) {
+    updateAgreeDisagreeCount(data.id, data.updatedAgreeCount, data.updatedDisagreeCount);
+}
+
+/** Update the counts of an insight, given new counts */
+function updateAgreeDisagreeCount(id, agreeCount, disagreeCount) {
+    $("#agreeCount_" + id).text(agreeCount);
+    $("#disagreeCount_" + id).text(disagreeCount);
+}
+
+
+
+// Overload jQuery error
+
+/** Called when an AJAX request returns an error */
+$("#error").ajaxError(function(event, request, settings){
+    $(this).text('Sorry, an error occured during last action.');
+});
