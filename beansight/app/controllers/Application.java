441b96ac1d69265b98d54a6edc25f8ced49aee36
steren <steren.giannini@gmail.com>
2010-10-27 15:05:57 +0000

    bugfix insight page when not logged
    git-svn-id: svn://92.243.5.254/srv/d_sterenfr/svn/beansight/Website/trunk@150 2e3b6b4b-2413-4334-92b7-7e2fc515c0b6

--- beansight/app/controllers/Application.java
+++ beansight/app/controllers/Application.java
@@ -35,50 +35,56 @@ import exceptions.UserIsAlreadyFollowingInsightException;
 
 public class Application extends Controller {
 
-	public static final int NUMBER_INSIGHTS_INDEXPAGE 		= 16;
-	public static final int NUMBER_INSIGHTS_USERPAGE 		= 6;
-	public static final int NUMBER_INSIGHTS_INSIGHTPAGE 	= 40;
-	public static final int NUMBER_EXPERTS_EXPERTPAGE 		= 16;
-	public static final int NUMBER_INSIGHTS_SEARCHPAGE		= 20;
-	public static final int NUMBER_EXPERTS_SEARCHPAGE		= 20;
-	
+	public static final int NUMBER_INSIGHTS_INDEXPAGE = 16;
+	public static final int NUMBER_INSIGHTS_USERPAGE = 6;
+	public static final int NUMBER_INSIGHTS_INSIGHTPAGE = 40;
+	public static final int NUMBER_EXPERTS_EXPERTPAGE = 16;
+	public static final int NUMBER_INSIGHTS_SEARCHPAGE = 20;
+	public static final int NUMBER_EXPERTS_SEARCHPAGE = 20;
+
 	public static void index() {
-		//TODO order by upDate
-		List<Insight> insights = Insight.find("order by creationDate DESC").fetch(NUMBER_INSIGHTS_INDEXPAGE);
-		
-		if(Security.isConnected()) {
+		// TODO order by upDate
+		List<Insight> insights = Insight.find("order by creationDate DESC")
+				.fetch(NUMBER_INSIGHTS_INDEXPAGE);
+
+		if (Security.isConnected()) {
 			User currentUser = CurrentUser.getCurrentUser();
 			List<Insight> insightActivity = currentUser.createdInsights;
 			List<Insight> followedInsights = currentUser.followedInsights;
 			List<User> followedUsers = currentUser.followedUsers;
-			render("Application/indexConnected.html", insights, followedInsights, followedUsers, insightActivity);
+			render("Application/indexConnected.html", insights,
+					followedInsights, followedUsers, insightActivity);
 		}
-		
+
 		render("Application/indexNotConnected.html", insights);
 	}
-	
-	public static void create(String insightContent, Date endDate, String tagLabelList, long categoryId) {
+
+	public static void create(String insightContent, Date endDate,
+			String tagLabelList, long categoryId) {
 		render(insightContent, endDate, tagLabelList, categoryId);
 	}
 
 	public static void myInsights() {
 		User currentUser = CurrentUser.getCurrentUser();
-		List<Insight> myLastInsights = currentUser.getLastInsights(NUMBER_INSIGHTS_USERPAGE);
-		
+		List<Insight> myLastInsights = currentUser
+				.getLastInsights(NUMBER_INSIGHTS_USERPAGE);
+
 		render(myLastInsights);
 	}
 
 	public static void insights(long categoryId) {
 		Category category = Category.findById(categoryId);
-		
+
 		String query = "";
-		if(categoryId != 0) {
-			query += "select i from Insight i join i.category c where c.id=" + categoryId;
+		if (categoryId != 0) {
+			query += "select i from Insight i join i.category c where c.id="
+					+ categoryId;
 		}
 		query += " order by creationDate DESC";
-		
-		List<Insight> insights = Insight.find(query).fetch(NUMBER_INSIGHTS_INSIGHTPAGE);
-		
+
+		List<Insight> insights = Insight.find(query).fetch(
+				NUMBER_INSIGHTS_INSIGHTPAGE);
+
 		User currentUser = CurrentUser.getCurrentUser();
 		List<Insight> followedInsights = currentUser.followedInsights;
 		render(insights, followedInsights, category);
@@ -86,45 +92,54 @@ public class Application extends Controller {
 
 	public static void experts() {
 		// TODO order by score
-		List<User> experts = User.find("order by crdate DESC").fetch(NUMBER_EXPERTS_EXPERTPAGE);
-		
+		List<User> experts = User.find("order by crdate DESC").fetch(
+				NUMBER_EXPERTS_EXPERTPAGE);
+
 		User currentUser = CurrentUser.getCurrentUser();
 		List<User> followedUsers = currentUser.followedUsers;
-		
+
 		render(experts, followedUsers);
 	}
-	
+
 	/**
 	 * create an insight for the current user
-	 * @param insightContent: the content of this insight (min 6, max 140 characters)
-	 * @param endDate: the end date chosen by the user
-	 * @param tagLabelList: a comma separated list of tags
-	 * @param categoryId: the ID of the category of the insight
+	 * 
+	 * @param insightContent
+	 *            : the content of this insight (min 6, max 140 characters)
+	 * @param endDate
+	 *            : the end date chosen by the user
+	 * @param tagLabelList
+	 *            : a comma separated list of tags
+	 * @param categoryId
+	 *            : the ID of the category of the insight
 	 */
-	public static void createInsight(@Required @MinSize(6) @MaxSize(140) String insightContent, @Required Date endDate, @MaxSize(100) String tagLabelList, @Required long categoryId) {
+	public static void createInsight(
+			@Required @MinSize(6) @MaxSize(140) String insightContent,
+			@Required Date endDate, @MaxSize(100) String tagLabelList,
+			@Required long categoryId) {
 		// Check if the given category Id corresponds to a category
 		Category category = Category.findById(categoryId);
-		if(category == null) {
+		if (category == null) {
 			validation.addError("categoryId", "Not a valid Category");
 			// FIXME: This error doesn't display
 		}
-	    if (validation.hasErrors()) {
-	        flash.error("Error creating the insight");
-	        create(insightContent, endDate, tagLabelList, categoryId);
-	    }
-		
+		if (validation.hasErrors()) {
+			flash.error("Error creating the insight");
+			create(insightContent, endDate, tagLabelList, categoryId);
+		}
+
 		User currentUser = CurrentUser.getCurrentUser();
-		Insight insight = currentUser.createInsight(insightContent, endDate, tagLabelList, categoryId);
+		Insight insight = currentUser.createInsight(insightContent, endDate,
+				tagLabelList, categoryId);
 
 		showInsight(insight.id);
 	}
-	
+
 	public static void displayAllInsights() {
 		List<Insight> allInsights = Insight.all().fetch();
 		render(allInsights);
 	}
-	
-	
+
 	/**
 	 * AJAX Agree a given insight
 	 * 
@@ -142,7 +157,7 @@ public class Application extends Controller {
 	public static void disagree(Long insightId) {
 		vote(insightId, State.DISAGREE);
 	}
-	
+
 	private static void vote(Long insightId, State voteState) {
 		// TODO : if not connected: go to log / signin page
 		User currentUser = CurrentUser.getCurrentUser();
@@ -157,39 +172,48 @@ public class Application extends Controller {
 		renderArgs.put("agree", voteState == State.AGREE);
 		render("Application/vote.json", insight);
 	}
-	
+
 	/**
 	 * Show info about a given insight
+	 * 
 	 * @param id
 	 */
-    public static void showInsight(Long id) {
-        Insight insight = Insight.findById(id);
-        notFoundIfNull(insight);
-		User currentUser = CurrentUser.getCurrentUser();
-        Vote lastUserVote = Vote.findLastVoteByUserAndInsight(currentUser.id, id);
-        List<Vote> lastVotes = insight.getLastVotes(5);
-        
-        render(insight, currentUser, lastUserVote, lastVotes);
-    }
-    
-    /**
-     * Show info about a given user
-     * @param id
-     */
-    public static void showUser(Long id) {
-    	User user = User.findById(id);
-        notFoundIfNull(user);              
-    	render(user);
-    }
-    
-    /**
-     * AJAX: Change the follow state for the connected user toward this insight
-     */
-    public static void toggleFollowingInsight(Long insightId) {
+	public static void showInsight(Long id) {
+		Insight insight = Insight.findById(id);
+		notFoundIfNull(insight);
+		
+		if (Security.isConnected()) {
+			User currentUser = CurrentUser.getCurrentUser();
+			Vote lastUserVote = Vote.findLastVoteByUserAndInsight(currentUser.id, id);
+			List<Vote> lastVotes = insight.getLastVotes(5);
+			
+			renderArgs.put("currentUser", currentUser);
+			renderArgs.put("lastUserVote", lastUserVote);
+			renderArgs.put("lastVotes", lastVotes);
+		}
+
+		render(insight);
+	}
+
+	/**
+	 * Show info about a given user
+	 * 
+	 * @param id
+	 */
+	public static void showUser(Long id) {
+		User user = User.findById(id);
+		notFoundIfNull(user);
+		render(user);
+	}
+
+	/**
+	 * AJAX: Change the follow state for the connected user toward this insight
+	 */
+	public static void toggleFollowingInsight(Long insightId) {
 		User currentUser = CurrentUser.getCurrentUser();
 		Insight insight = Insight.findById(insightId);
-		
-		if (currentUser.isFollowingInsight(insight)==true) {
+
+		if (currentUser.isFollowingInsight(insight) == true) {
 			currentUser.stopFollowingThisInsight(insightId);
 			renderArgs.put("follow", false);
 		} else {
@@ -200,35 +224,35 @@ public class Application extends Controller {
 				// it's ok to re-follow something
 			}
 		}
-		
+
 		render("Application/followInsight.json", insightId);
-    }
-    
-    public static void startFollowingInsight(Long insightId) {
+	}
+
+	public static void startFollowingInsight(Long insightId) {
 		User currentUser = CurrentUser.getCurrentUser();
-    	try {
+		try {
 			currentUser.startFollowingThisInsight(insightId);
 		} catch (UserIsAlreadyFollowingInsightException e) {
 			// it's ok to re-follow something
 		}
 		renderArgs.put("follow", true);
 		render("Application/followInsight.json", insightId);
-    }
+	}
 
-    public static void stopFollowingInsight(Long insightId) {
+	public static void stopFollowingInsight(Long insightId) {
 		User currentUser = CurrentUser.getCurrentUser();
 		currentUser.stopFollowingThisInsight(insightId);
 		renderArgs.put("follow", false);
 		render("Application/followInsight.json", insightId);
-    }
-    
-    /**
-     * AJAX: Change the follow state for the connected user toward this user
-     */
-    public static void toggleFollowingUser(Long userId) {
+	}
+
+	/**
+	 * AJAX: Change the follow state for the connected user toward this user
+	 */
+	public static void toggleFollowingUser(Long userId) {
 		User currentUser = CurrentUser.getCurrentUser();
 		User user = User.findById(userId);
-		if (currentUser.isFollowingUser(user)==true) {
+		if (currentUser.isFollowingUser(user) == true) {
 			currentUser.stopFollowingThisUser(user);
 			renderArgs.put("follow", false);
 		} else {
@@ -236,168 +260,178 @@ public class Application extends Controller {
 			renderArgs.put("follow", true);
 		}
 		render("Application/followUser.json", userId);
-    }
-    
-    /**
-     * Add a comment to a specific insight for the current user
-     * @param insightId: id of the insight
-     * @param content: text content of the insight
-     */
-    public static void addComment(Long insightId, String content) {
+	}
+
+	/**
+	 * Add a comment to a specific insight for the current user
+	 * 
+	 * @param insightId
+	 *            : id of the insight
+	 * @param content
+	 *            : text content of the insight
+	 */
+	public static void addComment(Long insightId, String content) {
 		User currentUser = CurrentUser.getCurrentUser();
-    	Insight insight = Insight.findById(insightId);
-    	Comment comment = insight.addComment(content, currentUser);
-    	
+		Insight insight = Insight.findById(insightId);
+		Comment comment = insight.addComment(content, currentUser);
+
 		render("Application/comment.json", comment);
-    }
-    
-    /**
-     * add tags to an insight
-     * @param insightId : the id of the tagged insight
-     * @param tagLabelList: a comma separated list of tag labels
-     */
+	}
+
+	/**
+	 * add tags to an insight
+	 * 
+	 * @param insightId
+	 *            : the id of the tagged insight
+	 * @param tagLabelList
+	 *            : a comma separated list of tag labels
+	 */
 	public static void addTags(Long insightId, String tagLabelList) {
 		User currentUser = CurrentUser.getCurrentUser();
 		Insight insight = Insight.findById(insightId);
-    	insight.addTags(tagLabelList, currentUser);
+		insight.addTags(tagLabelList, currentUser);
 		showInsight(insightId);
 	}
-	
+
 	/**
-	 * Get a list of all the categories of the website	
+	 * Get a list of all the categories of the website
 	 */
 	public static List<Category> getCategories() {
 		List<Category> categories = Category.findAll();
 		return categories;
 	}
-	
+
 	// TODO remove me, score computation should be called in a job.
 	public static void temp_recomputeScore() {
 		User currentUser = CurrentUser.getCurrentUser();
-    	currentUser.computeScores();
-    	currentUser.save();
-    	showUser(currentUser.id);
+		currentUser.computeScores();
+		currentUser.save();
+		showUser(currentUser.id);
 	}
 
-	
 	public static void saveSettings(User user) {
 		User currentUser = CurrentUser.getCurrentUser();
 		// User should be the same as the one connected
-	    if (currentUser.id.equals(user.id)==false) {
-	    	forbidden("It seems you are trying to hack someone else settings");
-	    }
+		if (currentUser.id.equals(user.id) == false) {
+			forbidden("It seems you are trying to hack someone else settings");
+		}
 		currentUser.avatar = user.avatar;
 		currentUser.save();
 		settings();
 	}
-	
-	
+
 	public static void settings() {
 		User user = CurrentUser.getCurrentUser();
 		render(user);
 	}
-	
+
 	/**
-	 * Render the user avatar 
+	 * Render the user avatar
+	 * 
 	 * @param userId
 	 */
 	public static void showAvatar(Long userId) {
 		User user = User.findById(userId);
 		if (user != null && user.avatar.isSet()) {
 			renderBinary(user.avatar.get());
-		} 
+		}
 		renderBinary(new File("public/images/unknown.jpg"));
 		notFound();
 	}
-	
+
 	/**
-	 * Receive an image from which the user want to crop his avatar.
-	 * The image is stored in the default attachments path with the name "tmpFile"+user.id
+	 * Receive an image from which the user want to crop his avatar. The image
+	 * is stored in the default attachments path with the name "tmpFile"+user.id
 	 * 
 	 * @param file
 	 */
 	public static void uploadUncropedAvatarImage(File file) {
 		User user = CurrentUser.getCurrentUser();
-		
-		File to = new File(FileAttachment.getStore(), "tmpFile"+user.id);
+
+		File to = new File(FileAttachment.getStore(), "tmpFile" + user.id);
 		Files.copy(file, to);
 
 		uploadAndCropAvatarScreen();
 	}
-	
+
 	/**
-	 * Render the uploaded image so that the user crop his avatar from it 
+	 * Render the uploaded image so that the user crop his avatar from it
 	 */
 	public static void displayUploadedTmpImage() {
 		User user = CurrentUser.getCurrentUser();
-		File tmpFile = new File(FileAttachment.getStore(), "tmpFile"+user.id);
+		File tmpFile = new File(FileAttachment.getStore(), "tmpFile" + user.id);
 		if (!tmpFile.exists()) {
 			notFound();
 		}
 		renderBinary(tmpFile);
-	}	
-	
+	}
+
 	/**
 	 * Render to the page which give the opportunity to upload an image to crop
 	 */
 	public static void uploadAndCropAvatarScreen() {
 		User user = CurrentUser.getCurrentUser();
-		
+
 		render(user);
 	}
-	
-	public static void cropImage(Integer x1, Integer y1, Integer x2, Integer y2, Integer imageW, Integer imageH) {
+
+	public static void cropImage(Integer x1, Integer y1, Integer x2,
+			Integer y2, Integer imageW, Integer imageH) {
 		User user = CurrentUser.getCurrentUser();
-		
-		File imageToCrop = new File(FileAttachment.getStore(), "tmpFile"+user.id);
+
+		File imageToCrop = new File(FileAttachment.getStore(), "tmpFile"
+				+ user.id);
 		try {
 			BufferedImage source = ImageIO.read(imageToCrop);
 			int originalImageWidth = source.getWidth();
 			int originalImageHeight = source.getHeight();
 			float ratioX = new Float(originalImageWidth) / imageW;
 			float ratioY = new Float(originalImageHeight) / imageH;
-		
-			Images.crop(imageToCrop, imageToCrop, Math.round(x1*ratioX), Math.round(y1*ratioY), Math.round(x2*ratioX), Math.round((y2*ratioY)));
+
+			Images.crop(imageToCrop, imageToCrop, Math.round(x1 * ratioX),
+					Math.round(y1 * ratioY), Math.round(x2 * ratioX),
+					Math.round((y2 * ratioY)));
 			user.avatar.set(imageToCrop);
 			user.saveAttachment();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public static void search(String query, int offset, long categoryId) {
-		if(query == null || query.isEmpty()) {
+		if (query == null || query.isEmpty()) {
 			insights(0);
 		}
 		Category category = Category.findById(categoryId);
-		
-		SearchResult result = Insight.search(query, offset, NUMBER_INSIGHTS_SEARCHPAGE, category);
-		
+
+		SearchResult result = Insight.search(query, offset,
+				NUMBER_INSIGHTS_SEARCHPAGE, category);
+
 		renderArgs.put("count", result.count);
 		renderArgs.put("insights", result.results);
 		render(query, category, offset);
 	}
-	
+
 	/**
 	 * AJAX : get more formatted results for a search
+	 * 
 	 * @return: the HTML containing the lines to add to the search results
 	 */
 	public static void moreSearch(String query, int offset, long categoryId) {
 		Category category = Category.findById(categoryId);
-		
-		SearchResult result = Insight.search(query, offset, NUMBER_INSIGHTS_SEARCHPAGE, category);
-		
+
+		SearchResult result = Insight.search(query, offset,
+				NUMBER_INSIGHTS_SEARCHPAGE, category);
+
 		renderArgs.put("insights", result.results);
 		render();
 	}
-	
-	
+
 	public static void userSearch(String query) {
 		Query q = Search.search(query, User.class);
 		List<User> users = q.fetch();
 		render(users);
 	}
-	
-	
+
 }
