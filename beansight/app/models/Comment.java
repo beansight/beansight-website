fe4e04969c8155fb0b10f24e37bd57bcdec408b0
steren <steren.giannini@gmail.com>
2010-12-03 19:17:26 +0000

    be carefull, "db" and ".settings" directory were versionned
    git-svn-id: svn://92.243.5.254/srv/d_sterenfr/svn/beansight/Website/trunk@185 2e3b6b4b-2413-4334-92b7-7e2fc515c0b6

--- beansight/app/models/Comment.java
+++ beansight/app/models/Comment.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 
 import play.db.jpa.Model;
@@ -12,6 +13,7 @@ import play.db.jpa.Model;
 @Entity
 public class Comment extends Model {
 
+	@Lob
 	public String content;
 
 	@ManyToOne
