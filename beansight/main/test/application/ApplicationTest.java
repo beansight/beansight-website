0ca8c3372238ddb0b57ef28f974fe2f2684ab6ba
steren <steren.giannini@gmail.com>
2011-02-09 11:53:45 +0000

    added one test to make sure the admin area is not accessible
    git-svn-id: svn://92.243.5.254/srv/d_sterenfr/svn/beansight/Website/trunk@437 2e3b6b4b-2413-4334-92b7-7e2fc515c0b6

--- beansight/main/test/application/ApplicationTest.java
+++ beansight/main/test/application/ApplicationTest.java
@@ -7,11 +7,18 @@ import play.test.FunctionalTest;
 public class ApplicationTest extends FunctionalTest {
 
     @Test
-    public void testThatIndexPageWorks() {
-        Response response = GET("/");
+    public void testThatWelcomePageWorks() {
+        Response response = GET("/welcome");
         assertIsOk(response);
         assertContentType("text/html", response);
         assertCharset("utf-8", response);
+    }
+    
+    @Test
+    public void testAdminSecurity() {
+        Response response = GET("/admin");
+        assertStatus(302, response);
+        assertHeaderEquals("Location", "http://localhost/login", response);
     }
     
 }
