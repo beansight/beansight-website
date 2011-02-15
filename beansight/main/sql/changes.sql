5066664434ab1af80015b5f56cb3a7b55fce91e6
jbclaramonte <jeanbaptiste.claramonte@gmail.com>
2011-02-15 13:26:53 +0000

    dropping and re creating ForgotenPassword table because it has been created without uppercase in production
    git-svn-id: svn://92.243.5.254/srv/d_sterenfr/svn/beansight/Website/trunk@470 2e3b6b4b-2413-4334-92b7-7e2fc515c0b6

--- beansight/main/sql/changes.sql
+++ beansight/main/sql/changes.sql
@@ -2,3 +2,13 @@
 -- Reset this page after each release and after having created a SVN tag
 --
 
+DROP TABLE forgotpassword;
+
+CREATE TABLE `ForgotPassword` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) DEFAULT NULL,
+  `crDate` datetime DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FORGOT_PWD_CODE_IDX` (`code`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
