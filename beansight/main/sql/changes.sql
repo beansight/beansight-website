1734c6d499a6f95e5b69c7bb2df5e0aa75b44afb
steren <steren.giannini@gmail.com>
2011-03-15 15:03:54 +0000

    applied changes.sql
    git-svn-id: svn://92.243.5.254/srv/d_sterenfr/svn/beansight/Website/trunk@673 2e3b6b4b-2413-4334-92b7-7e2fc515c0b6

--- beansight/main/sql/changes.sql
+++ beansight/main/sql/changes.sql
@@ -1,5 +1,3 @@
 -- Add to this file the database changes that need to be applied to the %playapps database
 -- Reset this page after each release and after having created a SVN tag
 --
-
-UPDATE `User` SET `invitationsLeft` = `invitationsLeft` + 5 WHERE `crdate` < '2011-03-16 12:00:00'
