1ef68d064cd4ed4c50ac1435e59446834b1beeb4
steren <steren.giannini@gmail.com>
2011-03-29 17:26:32 +0000

    bugfix in SQL.
    git-svn-id: svn://92.243.5.254/srv/d_sterenfr/svn/beansight/Website/trunk@738 2e3b6b4b-2413-4334-92b7-7e2fc515c0b6

--- beansight/main/sql/changes.sql
+++ beansight/main/sql/changes.sql
@@ -2,8 +2,8 @@
 -- Reset this page after each release and after having created a SVN tag
 --
 
-ALTER TABLE `InsightActivity` ADD COLUMN `totalCount` BIGINT(20)  NOT NULL AFTER `totalCount`;
-ALTER TABLE `InsightActivity` ADD COLUMN `newCommentCount` BIGINT(20)  NOT NULL AFTER `totalCount`;
+ALTER TABLE `InsightActivity` ADD COLUMN `totalCount` BIGINT(20)  NOT NULL;
+ALTER TABLE `InsightActivity` ADD COLUMN `newCommentCount` BIGINT(20)  NOT NULL;
 
 
 
