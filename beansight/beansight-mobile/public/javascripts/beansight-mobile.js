576576556a9915e2e72b9c84617e02450ba1013d
steren <steren.giannini@gmail.com>
2011-04-06 15:43:12 +0000

    load more insights mechanism
    git-svn-id: svn://92.243.5.254/srv/d_sterenfr/svn/beansight/Website/trunk@768 2e3b6b4b-2413-4334-92b7-7e2fc515c0b6

--- beansight/beansight-mobile/public/javascripts/beansight-mobile.js
+++ beansight/beansight-mobile/public/javascripts/beansight-mobile.js
@@ -1,3 +1,11 @@
+var INSIGH_TNUMBER_TO_LOAD = 10;
+
+var beansightConf = {};
+beansightConf.from = INSIGH_TNUMBER_TO_LOAD;
+beansightConf.sort = "incoming";
+
+var sort = "incomming"
+
 /** Current user agree an insight */
 function agree(insightUniqueId) {
     $.getJSON(agreeAction, {"insightUniqueId": insightUniqueId}, onVoteSuccess);
@@ -16,23 +24,38 @@ function onVoteSuccess(data) {
 }
 
 /** Load the insight list */
-function getInsights(sort) {
-    $.getJSON(getInsightsAction, {sort: sort}, onGetInsightsSuccess);
+function getInsights() {
+    $.getJSON(getInsightsAction, {sort: beansightConf.sort, number: INSIGH_TNUMBER_TO_LOAD}, onGetInsightsSuccess);
 }
 
 function onGetInsightsSuccess(data) {
 	$("#insightList")
 		.html( $("#insightTemplate").tmpl( data ) )
 		.listview('refresh'); 
+	associateInsightClickAction();
 	
+}
+
+function getMoreInsights() {
+    $.getJSON(getInsightsAction, {sort: beansightConf.sort, from: beansightConf.from, number: INSIGH_TNUMBER_TO_LOAD} ,onGetMoreInsightsSuccess);
+    beansightConf.from += INSIGH_TNUMBER_TO_LOAD;
+}
+
+function onGetMoreInsightsSuccess(data) {
+	$("#insightList")
+		.append( $("#insightTemplate").tmpl( data ) )
+		.listview('refresh'); 
+	associateInsightClickAction();	
+}
+
+function associateInsightClickAction() {
 	// associate to each one of these insights the click action
 	$(".insight-link").each(function(index, element) {
-		// Apply the insight info to the Insight page
 		// Get the insight info
 		var content = $(".content", element).html();
 		var endDate = $(".endDate", element).html();
-		var uniqueId = $(element).attr("data-uniqueid", uniqueId);
-		// change the content of the insight
+		var uniqueId = $(element).attr("data-uniqueid");
+		// change the content of the insight page
 		$(element).click(function() {
 			$("#page-insight").attr("data-uniqueid", uniqueId);
 			getInsight(uniqueId);
@@ -41,7 +64,6 @@ function onGetInsightsSuccess(data) {
 			$("#insight-content").html(content);
 			removeActiveVoteButton();
 		});
-		
 	});
 }
 
@@ -58,16 +80,6 @@ function setActiveVoteButton(voteState) {
 		$("#btn-disagree").addClass("ui-btn-active");
 	}
 }
-
-//function getMoreInsights() {
-//    $.getJSON(getInsightsAction, {from: 20} ,onGetMoreInsightsSuccess);
-//}
-//
-//function onGetMoreInsightsSuccess(data) {
-//	$("#insightList")
-//		.append( $("#insightTemplate").tmpl( data ) )
-//		.listview('refresh'); 
-//}
 
 /** Load the insight data */
 function getInsight(uniqueId) {
@@ -89,13 +101,19 @@ function onGetInsightSuccess(data) {
 $(document).ready(function() {
 	
 	$("#filter-upcomming").click(function() {
-		getInsights("incoming");
+		beansightConf.from = INSIGH_TNUMBER_TO_LOAD;
+		beansightConf.sort = "incoming";
+		getInsights();
 	});
 	$("#filter-popularity").click(function() {
-		getInsights("trending");
+		beansightConf.from = INSIGH_TNUMBER_TO_LOAD;
+		beansightConf.sort = "trending";		
+		getInsights();
 	})
 	$("#filter-update").click(function() {
-		getInsights("updated");
+		beansightConf.from = INSIGH_TNUMBER_TO_LOAD;		
+		beansightConf.sort = "updated";		
+		getInsights();
 	});
 	
 	$("#btn-agree").click(function() {
@@ -103,5 +121,9 @@ $(document).ready(function() {
 	});
 	$("#btn-disagree").click(function() {
 		disagree($("#page-insight").attr("data-uniqueid"));
+	});
+	
+	$("#btn-more").click(function() {
+		getMoreInsights();
 	});
 });
