553542c91a7dba8d2a14d1112453e95515915be2
steren <steren.giannini@gmail.com>
2011-04-06 17:03:39 +0000

    Login system
    git-svn-id: svn://92.243.5.254/srv/d_sterenfr/svn/beansight/Website/trunk@770 2e3b6b4b-2413-4334-92b7-7e2fc515c0b6

--- beansight/beansight-mobile/app/controllers/Mobile.java
+++ beansight/beansight-mobile/app/controllers/Mobile.java
@@ -8,16 +8,26 @@ import play.mvc.*;
 
 public class Mobile extends Controller {
 
-    public static void insights() {
-        render();
-    }
+	/**
+	 * The user needs to be connected to access the mobile version
+	 */
+	@Before(unless={"login"})
+	public static void forceConnect() {
+		if (!Security.isConnected()) {
+			login();
+		}
+	}
 
-    public static void login() {
-    	render();
-    }
-    
-    public static void create() {
-    	render();
-    }
-    
+	public static void insights() {
+		render();
+	}
+
+	public static void login() {
+		render();
+	}
+
+	// public static void create() {
+	// render();
+	// }
+
 }
