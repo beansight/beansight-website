72cd7c7b4d23ca64f550edf6974b3e0c227e2fd6
steren <steren.giannini@gmail.com>
2011-04-19 09:05:57 +0000

    Database models
    git-svn-id: svn://92.243.5.254/srv/d_sterenfr/svn/beansight/Website/trunk@804 2e3b6b4b-2413-4334-92b7-7e2fc515c0b6

--- beansight/main/sql/changes.sql
+++ beansight/main/sql/changes.sql
@@ -42,3 +42,34 @@ ALTER TABLE `UserCategoryScore` DROP FOREIGN KEY `FK6863460947140EFE`;
 ALTER TABLE `UserCategoryScore` DROP COLUMN `user_id`;
 
 update User set statusNewsletter = true;
+
+CREATE TABLE  `WeeklyMailingTask` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `attempt` int(11) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `language` varchar(255) DEFAULT NULL,
+  `sendTo` varchar(255) DEFAULT NULL,
+  `sent` bit(1) NOT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKF56DAB8F47140EFE` (`user_id`),
+  CONSTRAINT `FKF56DAB8F47140EFE` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `WeeklyMailingTask_NextWeekInsights` (
+  `WeeklyMailingTask_id` bigint(20) NOT NULL,
+  `nextWeekInsights_id` bigint(20) NOT NULL,
+  KEY `FK1BA2BD52F26200B6` (`WeeklyMailingTask_id`),
+  KEY `FK1BA2BD52E7041C2C` (`nextWeekInsights_id`),
+  CONSTRAINT `FK1BA2BD52E7041C2C` FOREIGN KEY (`nextWeekInsights_id`) REFERENCES `Insight` (`id`),
+  CONSTRAINT `FK1BA2BD52F26200B6` FOREIGN KEY (`WeeklyMailingTask_id`) REFERENCES `WeeklyMailingTask` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `WeeklyMailingTask_PreviousWeekInsights` (
+  `WeeklyMailingTask_id` bigint(20) NOT NULL,
+  `previousWeekInsights_id` bigint(20) NOT NULL,
+  KEY `FK2DD51F56F26200B6` (`WeeklyMailingTask_id`),
+  KEY `FK2DD51F56D70563A8` (`previousWeekInsights_id`),
+  CONSTRAINT `FK2DD51F56D70563A8` FOREIGN KEY (`previousWeekInsights_id`) REFERENCES `Insight` (`id`),
+  CONSTRAINT `FK2DD51F56F26200B6` FOREIGN KEY (`WeeklyMailingTask_id`) REFERENCES `WeeklyMailingTask` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
