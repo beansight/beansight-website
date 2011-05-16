e61c5f36b0d3c002378c7ad0ae639024cd482d91
steren <steren.giannini@gmail.com>
2011-05-16 18:30:05 +0000

    Event : selected insights now with a tagline + bugfix share button + go to topic.
    git-svn-id: svn://92.243.5.254/srv/d_sterenfr/svn/beansight/Website/trunk@884 2e3b6b4b-2413-4334-92b7-7e2fc515c0b6

--- beansight/main/app/models/Event.java
+++ beansight/main/app/models/Event.java
@@ -25,37 +25,74 @@ public class Event extends Model {
 	
 	@Lob
 	public String message;
-	
 	@Lob
 	public String callToAction;
+
+	public String shareCallToAction;
 	
 	@Index(name = "EVENT_UNIQUE_ID_IXD")
 	public String uniqueId;
 	
+	
 	public String imageThumbURL;
-
 	public String imageBackgroundLeftURL;
-
 	public String imageBackgroundRightURL;
 	
-	@ManyToMany(cascade = CascadeType.ALL)
-	public List<Insight> insights;
+	@ManyToOne
+	public Topic topic;
+	
+	@ManyToOne
+	public Insight insight1;
+	public String insight1Tagline;
+	@ManyToOne
+	public Insight insight2;
+	public String insight2Tagline;
+	@ManyToOne
+	public Insight insight3;
+	public String insight3Tagline;
+	@ManyToOne
+	public Insight insight4;
+	public String insight4Tagline;
+	@ManyToOne
+	public Insight insight5;
+	public String insight5Tagline;
+	
 
 	public Date creationDate;
 	public Date endDate;
 	
-	public Event(String title, String message, String callToAction, String uniqueId, List<Insight> insights) {
-		this.title = title;
-		this.message = message;
-		this.callToAction = callToAction;
-		this.uniqueId = uniqueId;
-		this.insights = insights;
-		this.creationDate = new Date();
-		this.endDate = new Date();
+	public List<InsightLine> getInsights() {
+		List<InsightLine> list = new ArrayList<InsightLine>();
+		if(insight1 != null) {
+			list.add(new InsightLine(insight1, insight1Tagline));
+		}
+		if(insight2 != null) {
+			list.add(new InsightLine(insight2, insight2Tagline));
+		}
+		if(insight3 != null) {
+			list.add(new InsightLine(insight3, insight3Tagline));
+		}
+		if(insight4 != null) {
+			list.add(new InsightLine(insight4, insight4Tagline));
+		}
+		if(insight5 != null) {
+			list.add(new InsightLine(insight5, insight5Tagline));
+		}
+		return list;
 	}
 	
 	public String toString() {
 	    return title;
 	}
-
+	
+	public class InsightLine {
+		public Insight insight;
+		public String tagLine;
+		public InsightLine(Insight insight, String tagLine) {
+			this.insight = insight;
+			this.tagLine = tagLine;
+		}
+		
+	}
+	
 }
