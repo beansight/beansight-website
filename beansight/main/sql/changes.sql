1e3d43a19838126378f5a2ac47546c92eda451e6
jbclaramonte <jeanbaptiste.claramonte@gmail.com>
2011-06-03 10:48:50 +0000

    reset
    git-svn-id: svn://92.243.5.254/srv/d_sterenfr/svn/beansight/Website/trunk@921 2e3b6b4b-2413-4334-92b7-7e2fc515c0b6

--- beansight/main/sql/changes.sql
+++ beansight/main/sql/changes.sql
@@ -1,8 +1,3 @@
 -- Add to this file the database changes that need to be applied to the %playapps database
 -- Reset this page after each release and after having created a SVN tag
 --
-
-ALTER TABLE `FeaturedTopic`
-    ADD COLUMN `language_id` bigint(20) DEFAULT NULL,
-    ADD KEY `FKF0B39581F53795DE` (`language_id`),
-    ADD CONSTRAINT `FKF0B39581F53795DE` FOREIGN KEY (`language_id`) REFERENCES `Language` (`id`);
