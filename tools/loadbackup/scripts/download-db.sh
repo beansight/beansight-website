6b5c43d5e058eac477029beb6cf18364f9b69b43
jeanbaptiste <jeanbaptiste.claramonte@gmail.com>
2012-07-04 23:37:35 +0200

    [GANDI] fix the script to download the mysql database dump

--- tools/loadbackup/scripts/download-db.sh
+++ tools/loadbackup/scripts/download-db.sh
@@ -4,4 +4,5 @@ rm -r ./backup_prod
 mkdir backup_prod
 
 echo 'retrieving current db backup from prod and copying to ./backup_prod'
-scp -i ~/.ssh/prod@beansight.com  -r play@92.243.16.31:/backup/current/mysql-dump.gz ./backup_prod/mysql-dump.gz
+#scp -i ~/.ssh/prod@beansight.com  -r play@92.243.16.31:/backup/current/mysql-dump.gz ./backup_prod/mysql-dump.gz
+scp admin@92.243.4.73:/home/admin/backup/hourly/dump.sql.gz ./backup_prod/mysql-dump.gz
