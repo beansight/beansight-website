e796d0aee47ffff06b757e66992910fd71947bd7
jeanbaptiste <jeanbaptiste.claramonte@gmail.com>
2013-03-25 17:33:50 +0100

    split beansight.tar.gz before upload to dropbox

--- tools/crontabJobs/backup.sh
+++ tools/crontabJobs/backup.sh
@@ -13,8 +13,22 @@ echo "creating backup of beansight app and more important: attachments"
 tar -czf $destDir/beansight.tar.gz ~/beansight-website/beansight
 
 echo "copy db backup to dropbox"
-$dropboxScript/dropbox_uploader.sh upload $destDir/dump.sql.gz dump.sql.gz
+$dropboxScript/dropbox_uploader.sh upload $destDir/dump.sql.gz
 
 echo "copy beansight backup to dropbox"
-$dropboxScript/dropbox_uploader.sh upload $destDir/beansight.tar.gz beansight.tar.gz
+
+echo "first splitting beansight.tar.gz in 6 files ... "
+rm -rf "$destDir/beansight_split"
+mkdir "$destDir/beansight_split"
+
+split -n 6 "$destDir/beansight.tar.gz" "$destDir/beansight_split/beansight.tar.gz."
+
+
+for i in "$destDir"/beansight_split/*
+do
+	echo "uploading: $i"
+	$dropboxScript/dropbox_uploader.sh upload $i
+done;
+
+#$dropboxScript/dropbox_uploader.sh upload $destDir/beansight.tar.gz beansight.tar.gz
 
